82433b7cbf1ae5e9fddfa88f36a549326bfd8214
venkatp227 <venkatp227@gmail.com>
2018-05-11 23:24:19 +0100

    my config

--- ecs.tf
+++ ecs.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-west-2"
+  profile = "mytf"
 }
 
 module "ecs" {
@@ -15,15 +16,12 @@ module "ecs" {
   max_size             = "${var.max_size}"
   min_size             = "${var.min_size}"
   desired_capacity     = "${var.desired_capacity}"
-  key_name             = "${aws_key_pair.ecs.key_name}"
+  key_name             = "${var.aws_keypair}"
   instance_type        = "${var.instance_type}"
   ecs_aws_ami          = "${var.ecs_aws_ami}"
 }
 
-resource "aws_key_pair" "ecs" {
-  key_name   = "ecs-key-${var.environment}"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtMljjj0Ccxux5Mssqraa/iHHxheW+m0Rh17fbd8t365y9EwBn00DN/0PjdU2CK6bjxwy8BNGXWoUXiSDDtGqRupH6e9J012yE5kxhpXnnkIcLGjkAiflDBVV4sXS4b3a2LSXL5Dyb93N2GdnJ03FJM4qDJ8lfDQxb38eYHytZkmxW14xLoyW5Hbyr3SXhdHC2/ecdp5nLNRwRWiW6g9OA6jTQ3LgeOZoM6dK4ltJUQOakKjiHsE+jvmO0hJYQN7+5gYOw0HHsM+zmATvSipAWzoWBWcmBxAbcdW0R0KvCwjylCyRVbRMRbSZ/c4idZbFLZXRb7ZJkqNJuy99+ld41 ecs@aws.fake"
-}
+variable "aws_keypair" { default = "myawskey" }
 
 variable "vpc_cidr" {}
 variable "environment" {}
--- ecs.tfvars
+++ ecs.tfvars
@@ -6,7 +6,7 @@ public_subnet_cidrs = ["10.0.0.0/24", "10.0.1.0/24"]
 
 private_subnet_cidrs = ["10.0.50.0/24", "10.0.51.0/24"]
 
-availability_zones = ["eu-west-1a", "eu-west-1b"]
+availability_zones = ["eu-west-2a", "eu-west-2b"]
 
 max_size = 1
 
@@ -16,4 +16,4 @@ desired_capacity = 1
 
 instance_type = "t2.micro"
 
-ecs_aws_ami = "ami-95f8d2f3"
+ecs_aws_ami = "ami-2218f945"
